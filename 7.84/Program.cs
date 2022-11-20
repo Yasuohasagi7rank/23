0cd6d55c29a5f86a1465a406aab43c0d221e9012
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:15:22 +0300

    Add files via upload

---
+++ 7.84/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7._84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdm = new Random();
+
+            Console.Write("Рандомные числа: ");
+
+            double a;
+            double b = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                a = rdm.NextDouble() * 20;
+                b += a;
+                Console.Write($"{a:f2} ");
+            }
+            Console.WriteLine(b > 100.78 ? "\nСумма чисел превышает 100,78" : "\nСумма чисел НЕ превышает 100,78");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+    
+}
